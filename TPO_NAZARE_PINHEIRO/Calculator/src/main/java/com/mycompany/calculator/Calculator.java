08d16d8059ec6e25c993719bb8d6253baeaa6c46
ana_rita nazare_pinheiro <anarita.nazarepinheiro@epfedu.fr>
2023-09-25 11:12:05 +0200

    TPO calculator de Ana Rita NAZARE PINHEIRO TD C, FG 2

---
+++ TPO_NAZARE_PINHEIRO/Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author anaza
+ */
+/* NAZARE PINHEIRO, TP0, 
+Calcultator, 
+24/09/23
+*/
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Please enter the operator : \n 1) add \n 2) substract \n 3) multiply \n 4) divide \n 5) modulo");
+        int operateur=sc.nextInt();
+        
+        if (operateur<0 || operateur>=5) {
+            System.out.println("error, chose an operator between 1 and 5");
+            System.exit(0);
+        } else {
+        
+            System.out.println("\n Please enter the first number :");
+            double operande1 = sc.nextInt();
+
+            System.out.println("\n Please enter the second number :");
+            double operande2 = sc.nextInt();
+
+            double result=0;      
+            if (operateur==1) {
+                result = operande1 + operande2;
+            } else { 
+                if (operateur==2) { 
+                    result = operande1 - operande2;
+                } else { 
+                    if (operateur==3) {
+                        result = operande1*operande2;
+                    } else {
+                        if (operateur==4){
+                            result = operande1/operande2;
+                        } else {
+                            result = operande1%operande2;
+                        }
+                    }
+                }
+            }
+
+            System.out.println(result);
+
+        }}
+}
