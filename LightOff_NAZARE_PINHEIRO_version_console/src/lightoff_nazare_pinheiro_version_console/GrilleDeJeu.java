2ed0a13ab104ddcd09324bb81cbe6bfdf8aa10b5
ana_rita nazare_pinheiro <anarita.nazarepinheiro@epfedu.fr>
2023-10-24 13:03:21 +0200

    Projet Light Off de Ana Rita NAZARE PINHEIRO TD C, FG 2

---
+++ LightOff_NAZARE_PINHEIRO_version_console/src/lightoff_nazare_pinheiro_version_console/GrilleDeJeu.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_nazare_pinheiro_version_console;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anaza
+ */
+public class GrilleDeJeu {
+    int nbLignes;
+    int nbColonnes; 
+    CelluleLumineuse[][] matriceCellules = new CelluleLumineuse[nbLignes][nbColonnes];
+
+    public GrilleDeJeu(int nbLignes, int nbColonnes) {
+        this.nbLignes = nbLignes;
+        this.nbColonnes = nbColonnes;
+    }
+    
+    
+    
+    
+}
